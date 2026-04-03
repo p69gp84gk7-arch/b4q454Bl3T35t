b5ef14fec698c6c7c82d871a426f0a6777c9bd66
p69gp84gk7-arch <p69gp84gk7@privaterelay.appleid.com>
2026-04-03 13:09:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 proj4.defs("EPSG:2154","+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +units=m +no_defs");
 
 const map = L.map('map', { doubleClickZoom: false }).setView([42.7645, 0.5833], 15);
+preferCanvas: true
 const satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', { maxZoom: 19 }).addTo(map);
 const planOSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 });
 L.control.layers({ "🌍 Satellite": satellite, "🗺️ Plan": planOSM }).addTo(map);
